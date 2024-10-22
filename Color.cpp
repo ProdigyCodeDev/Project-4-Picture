20ffaeaf0f1086c65e9e1ab9fce6706f608a25c9
Christopher Purnawan <109975219+ProdigyCodeDev@users.noreply.github.com>
2024-10-21 22:48:04 -0400

    Update Color.cpp
    // broken ins and outs

--- Color.cpp
+++ Color.cpp
@@ -16,7 +16,70 @@
 // TODO: implement first checkRange, then two constructors, setRed, getRed,
 //       setGreen, getGreen, setBlue, getBlue, read, write.
 
+Color::Color() {
+	int red = 0;
+	int green = 0;
+	int blue = 0;
+}
 
+Color::Color(int redVal, int greenVal, int blueVal) {
+	Color::setRed(redVal);
+	Color::setGreen(greenVal);
+	Color::setBlue(blueVal);
+}
+
+void Color::setRed(int redVal) {
+	red = checkRange(redVal);
+}
+
+int Color::getRed() {
+	return red;
+}
+
+
+void Color::setGreen(int greenVal) {
+	green = checkRange(greenVal);
+}
+
+int Color::getGreen() {
+	return green;
+}
+
+void Color::setBlue(int blueVal) {
+	blue = checkRange(blueVal);
+}
+
+int Color::getBlue() {
+	return blue;
+}
+
+void Color::read(istream& ins) {
+	ins >> red >> green >> blue;
+}
+/**
+   * Requires: outs is in good state.
+   * Modifies: outs.
+   * Effects:  Writes color in form red  green  blue
+   * Example:  100  255  125
+   */
+
+void Color::write(ostream& outs) {
+	outs << red << " " << green << " " << blue << endl;
+}
+
+int Color::checkRange(int val) {
+	if (val >= 0 && val <= 255) {
+		return val;
+	}
+	else{
+		if (val < 0) {
+			return 0;
+		}
+		else if (val > 255) {
+			return 255;
+		}
+	}
+}
 
 // Your code goes above this line.
 // Don't change the implementations below!
@@ -32,3 +95,4 @@ ostream& operator << (ostream& outs, Color color)
    color.write(outs);
    return outs;
 }
+
