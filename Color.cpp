eaa5e5a92dfcedda8565d6c64f6b317b6792e53a
ChnnL <lichenlc@umich.edu>
2024-10-27 13:48:32 -0400

    Update Color.cpp

--- Color.cpp
+++ Color.cpp
@@ -5,83 +5,85 @@
  * EECS 183
  * Project 4: CoolPics
  *
- * <#Name(s)#>
- * <#uniqname(s)#>
+ * Chen Li, Christopher Purnawan
+ * lichenlc, chrislp
  *
- * <#Description#>
+ * The implementations of Color class.
  */
 
 #include "Color.h"
 
- // TODO: implement first checkRange, then two constructors, setRed, getRed,
- //       setGreen, getGreen, setBlue, getBlue, read, write.
-
+// Initializing color values to 0.
 Color::Color() {
-	int red = 0;
-	int green = 0;
-	int blue = 0;
+    int red = 0;
+    int green = 0;
+    int blue = 0;
 }
 
+// Initializing color with specified values.
 Color::Color(int redVal, int greenVal, int blueVal) {
-	Color::setRed(redVal);
-	Color::setGreen(greenVal);
-	Color::setBlue(blueVal);
+    Color::setRed(redVal);
+    Color::setGreen(greenVal);
+    Color::setBlue(blueVal);
 }
 
+// Sets the red component after checking it is within 0-255.
 void Color::setRed(int redVal) {
-	red = checkRange(redVal);
+    red = checkRange(redVal);
 }
 
+// Returns the red component of the color.
 int Color::getRed() {
-	return red;
+    return red;
 }
 
-
+// Sets the green component after checking it is within 0-255.
 void Color::setGreen(int greenVal) {
-	green = checkRange(greenVal);
+    green = checkRange(greenVal);
 }
 
+// Returns the green component of the color.
 int Color::getGreen() {
-	return green;
+    return green;
 }
 
+// Sets the blue component after checking it is within 0-255.
 void Color::setBlue(int blueVal) {
-	blue = checkRange(blueVal);
+    blue = checkRange(blueVal);
 }
 
+// Returns the blue component of the color.
 int Color::getBlue() {
-	return blue;
+    return blue;
 }
 
+// Reads color values from an input stream and ensures they are within 0-255.
 void Color::read(istream& ins) {
-	ins >> red >> green >> blue;
-	red = checkRange(red);
-	green = checkRange(green);
-	blue = checkRange(blue);
+    ins >> red >> green >> blue;
+    red = checkRange(red);
+    green = checkRange(green);
+    blue = checkRange(blue);
 }
-/**
-   * Requires: outs is in good state.
-   * Modifies: outs.
-   * Effects:  Writes color in form red  green  blue
-   * Example:  100  255  125
-   */
 
+// Writes color values to an output stream.
 void Color::write(ostream& outs) {
-	outs << red << " " << green << " " << blue << endl;
+    outs << red << " " << green << " " << blue << endl;
 }
 
+// Ensures a color component value is within 0-255.
+// If out of range, make it to the nearest valid value (0 or 255)
 int Color::checkRange(int val) {
-	if (val >= 0 && val <= 255) {
-		return val;
-	}
-	else {
-		if (val < 0) {
-			return 0;
-		}
-		else if (val > 255) {
-			return 255;
-		}
-	}
+    if (val >= 0 && val <= 255) {
+        return val;
+    }
+    else{
+        if (val < 0) {
+            return 0;
+        }
+        else if (val > 255) {
+            return 255;
+        }
+    }
 }
 
 // Your code goes above this line.
@@ -89,12 +91,12 @@ int Color::checkRange(int val) {
 
 istream& operator >> (istream& ins, Color& color)
 {
-	color.read(ins);
-	return ins;
+   color.read(ins);
+   return ins;
 }
 
 ostream& operator << (ostream& outs, Color color)
 {
-	color.write(outs);
-	return outs;
+   color.write(outs);
+   return outs;
 }
